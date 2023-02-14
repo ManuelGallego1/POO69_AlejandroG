97a3d19ea69e2d4dfead0c20c68c6528cfcf3ab0
ManuelGallego1 <manuela.gallegoa@autonoma.edu.co>
2023-02-14 11:55:59 -0500

    update

---
+++ 00-template.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP Files</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="min-h-[100vh] flex justify-center items-center">
+    <main class="w-[300px] h-[600px] bg-black/20 rounded text-white">
+        <heade class="flex justify-center items-center text-2xl font-mono border-b border-blue-200 border-dashed pb-3">
+            <h1> Main Menu (OOP)</h1>
+        </header>
+        <section>
+            
+        </section>
+    </main>
+    <script src="js/tailwild-3.2.4.js"></script>
+</body>
+</html>
